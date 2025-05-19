0280c7142fa0e1448fbee0fe5beca1ae9d03763d
Dinh Quan <quanvonguyendinh@gmail.com>
2025-05-19 19:43:55 +0700

    feat: config db

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,9 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const connect = async () => {
   try {
+    await mongoose.connect(process.env.DATABASE_URI);
     console.log('connect successfully!');
   } catch (error) {
     console.error('connect failure!');
