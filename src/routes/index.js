1e9a78bf85f12828b1a6345f8c72645835959020
Dinh Quan <quanvonguyendinh@gmail.com>
2025-05-21 14:51:33 +0700

    fix: remove import example file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,6 @@
-const exampleRouter = require('./example.routes');
 const authController = require('./auth.routes');
 
 const routes = (app) => {
-  app.use(`/api/examples`, exampleRouter);
   app.use(`/api/auth`, authController);
 };
 
